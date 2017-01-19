ba4e60546e5a49c6858f7c498c8cd4b6173b96d7
Alexandrehebertvincent <alexandre.hebertvincent@gmail.com>
2017-01-19 15:17:07 -0500

    var modif

--- config/env_var.sh
+++ config/env_var.sh
@@ -1,3 +1,4 @@
 export TSAP_DATABASEDRIVER="postgres"
 export TSAP_CONNECTIONSTRING="host=localhost user=postgres dbname=tsapBack sslmode=disable password=alex1997"
-export TSAP_PORT= ":3000"
+export TSAP_PORT=":3000"
+export TSAP_DEBUG="true"
