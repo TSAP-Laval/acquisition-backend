9ea62759a73a2ea334abc70a9a28392c85bb5238
Alexandre Hébert-Vincent <alexandre.hebertvincent@gmail.com>
2017-01-19 21:18:28 -0500

    Video (#14)
    * Delete .DS_Store
    
    * Delete .DS_Store
    
    * Update README.md
    
    * Update README.md
    
    * Tests some routes
    
    * debug needed
    
    * script
    
    * bd
    
    * Updated http router to Mux and added envconfig
    
    * Renamed APIURL to PORT
    
    * Fix
    
    * Starting video upload POST
    
    * Fix uppercase
    
    * twt
    
    * ajoutBD
    
    * backend edit
    
    * fix
    
    * Uploaded video is done
    
    * Refactor code
    
    * Release V1

--- config/env_var.sh
+++ config/env_var.sh
@@ -1,4 +1,4 @@
 export TSAP_DATABASEDRIVER="postgres"
-export TSAP_CONNECTIONSTRING="host=localhost user=postgres dbname=tsapBack sslmode=disable password=alex1997"
-export TSAP_PORT=":3000"
+export TSAP_CONNECTIONSTRING="host=localhost user=postgres dbname=tsap_acquisition sslmode=disable password="
+export TSAP_PORT= ":3000"
 export TSAP_DEBUG="true"
