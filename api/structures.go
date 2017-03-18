e05b3fab6abbeac0d86591b36cc8277fb22a15df
laurentlp <laurentlp@users.noreply.github.com>
2017-03-18 16:55:16 -0400

    Multiple uploads (#57)
    * commit
    
    * test
    
    * Formating
    
    * backend partie gestion
    
    * endpoint
    
    * Ajout des endpoints pour les terrains
    
    * Added all team endpoints and fix endpoints name
    
    * Added error handler
    
    * Fix endpoint name
    
    * Fix team and field func
    
    * Ajout des tests
    
    * Fix
    
    * Updated circleci to include api tests
    
    * Second try
    
    * Seed the DB first to make tests pass
    
    * Retry
    
    * Fix wrong db driver config
    
    * Some tests
    
    * Again
    
    * And again
    
    * Test
    
    * Maybe
    
    * OK
    
    * Test again
    
    * Test
    
    * Converted fields in ENGLISH as it is in the DB script
    
    * Fix some errors
    
    * Possibly fix tests
    
    * Factoriser le code
    
    * Added games endpoint
    
    * Fix
    
    * Fix upload logic, refactor...
    
    * Update structures.go
    
    * Added teams creation into the seeder
    
    * Might fix circle
    
    * Test again
    
    * modif joueur
    
    * Fix gofmt
    
    * Try fix circle
    
    * Test
    
    * Fix deploy
    
    * Mise à jour des structures
    
    * Updated structures, added multiple files upload and part parser.... tests missing
    
    * Updated structure and adapted seeder. Finished multiple uploads
    
    * Fix tests
    
    * Added issues and PR templates
    
    * Fix part when no parts and video completed type
    
    * Updated structure

--- api/structures.go
+++ api/structures.go
@@ -63,32 +63,53 @@ type Players struct {
 // Locations les lieux
 type Locations struct {
 	gorm.Model
-	Name    string
-	City    string
-	Address string
+	Name          string
+	City          string
+	Address       string
+	InsideOutside string
+	FieldType     FieldTypes
+	FieldTypesID  int
+}
+
+// FieldTypes les types de terrains
+type FieldTypes struct {
+	gorm.Model
+	Type        string
+	Description string
 }
 
 // Videos les videos
 type Videos struct {
 	gorm.Model
 	Path      string
-	Completed bool
+	Part      int
+	Completed int
+	Game      Games
+	GameID    int
 }
 
 // Games les parties
 type Games struct {
 	gorm.Model
-	HomeTeam     Teams
-	HomeTeamID   int
-	OpposingTeam string
-	Season       Seasons
-	SeasonID     int
-	Location     Locations
-	LocationID   int
-	Video        Videos
-	VideoID      int
-	Date         string
-	Action       []Actions
+	HomeTeam       Teams
+	HomeTeamID     int
+	OpposingTeam   string
+	Season         Seasons
+	SeasonID       int
+	Location       Locations
+	LocationID     int
+	FieldCondition string
+	Temperature    Temperatures
+	TemperatureID  int
+	Date           string
+	Action         []Actions
+}
+
+// Temperatures la température durant la partie
+type Temperatures struct {
+	gorm.Model
+	TemperatureType string // Rain, wind, sun, etc
+	Degree          string // The temperature un degree celcius
 }
 
 // Positions les positions des joueurs
--- api/upload.go
+++ api/upload.go
@@ -4,6 +4,10 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/jinzhu/gorm"
 )
@@ -12,47 +16,85 @@ import (
 func (a *AcquisitionService) UploadHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		file, handler, err := r.FormFile("file")
-		defer file.Close()
-
-		a.ErrorHandler(w, err)
 		db, err := gorm.Open(a.config.DatabaseDriver, a.config.ConnectionString)
 		defer db.Close()
-
 		a.ErrorHandler(w, err)
 
-		var v Videos
-		v.Completed = false
-		v.Path = "home/tsap/api/videos/" + handler.Filename
+		err = r.ParseMultipartForm(8589934592) // 8Gb
+		a.ErrorHandler(w, err)
 
-		if db.NewRecord(v) {
-			db.Create(&v)
-			if db.NewRecord(v) {
-				msg := map[string]string{"error": "Une erreur est survenue lors de la création de la video dans la base de données. Veuillez réessayer!"}
-				a.Error(msg["error"])
-				Message(w, msg, http.StatusInternalServerError)
-			} else {
-				// On regarde si le dossier videos existe déjà.
-				// Dans le cas contraire, on le crée
-				if _, err := os.Stat("./videos/"); os.IsNotExist(err) {
-					os.MkdirAll("./videos/", 0777)
+		formdata := r.MultipartForm
+
+		if _, err := os.Stat("./videos/"); os.IsNotExist(err) {
+			os.MkdirAll("./videos/", 0777)
+		}
+
+		files := formdata.File["file"]
+
+		var g Games
+		db.Create(&g)
+
+		for i := range files {
+			fileSplit := strings.Split(files[i].Filename, ".")
+			ext := fileSplit[len(fileSplit)-1]
+
+			var v Videos
+			if len(files) > 1 {
+				re := regexp.MustCompile(`\((.*?)\)`)
+				part := re.FindStringSubmatch(files[i].Filename)[1]
+				p, err := strconv.ParseInt(part, 10, 0)
+
+				if err != nil {
+					msg := map[string]string{"error": "La nomenlature des fichiers est incorrecte! Veuillez vous assurer qu'ils contiennent un (#)!"}
+					a.Error(msg["error"])
+					Message(w, msg, http.StatusInternalServerError)
 				}
 
-				f, err := os.OpenFile("./videos/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-				defer f.Close()
-
-				a.ErrorHandler(w, err)
-
-				io.Copy(f, file)
-
-				msg := map[string]string{"succes": "Le video a été envoyé avec succès!"}
-				Message(w, msg, http.StatusCreated)
+				// Ajout de la partie
+				v.Part = int(p)
+			} else {
+				v.Part = 1
 			}
-		} else {
-			msg := map[string]string{"error": "Une vidéo avec le même nom existe déjà. Veuillez renommer cette vidéo."}
-			Message(w, msg, http.StatusInternalServerError)
-			return
+
+			// Le timestamp sera le nom du fichier
+			timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+
+			filename := timestamp + "." + ext
+
+			v.Completed = 0
+			v.Path = "home/tsap/api/videos/" + filename
+			v.Game = g
+
+			if db.NewRecord(v) {
+				db.Create(&v)
+				if db.NewRecord(v) {
+					msg := map[string]string{"error": "Une erreur est survenue lors de la création de la video dans la base de données. Veuillez réessayer!"}
+					a.Error(msg["error"])
+					Message(w, msg, http.StatusInternalServerError)
+				} else {
+					// On regarde si le dossier videos existe déjà.
+					// Dans le cas contraire, on le crée
+					file, err := files[i].Open()
+					defer file.Close()
+					a.ErrorHandler(w, err)
+
+					out, err := os.Create("./videos/" + filename)
+
+					defer out.Close()
+					a.ErrorHandler(w, err)
+
+					_, err = io.Copy(out, file)
+
+					a.ErrorHandler(w, err)
+				}
+			} else {
+				msg := map[string]string{"error": "Une vidéo avec le même nom existe déjà. Veuillez renommer cette vidéo."}
+				Message(w, msg, http.StatusInternalServerError)
+				return
+			}
 		}
+		msg := map[string]string{"succes": "Video(s) envoyé(s) avec succès!", "game_id": strconv.Itoa(int(g.ID))}
+		Message(w, msg, http.StatusCreated)
 	case "OPTIONS":
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.WriteHeader(http.StatusOK)
