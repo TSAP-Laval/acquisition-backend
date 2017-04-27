3988a27ab8daec0e1cbae8837e9496eae2fff56f
Mehdi Laribi <m.laribi@hotmail.com>
2017-04-27 14:37:59 -0400

    Ajout tests

--- api/coachs_test.go
+++ api/coachs_test.go
@@ -23,7 +23,8 @@ func PostNewCoach(t *testing.T) {
 			"Fname": "MEHEHHEHEHEDi", 
 			"Lname": "Lariihhhiibi", 
 			"Actif": "true", 
-			"Email": "Mehdi@hotmale.com"
+			"Email": "Mehdi@hotmale.com",
+			"SeasonID": "1"
 		}`)
 
 	request, err := http.NewRequest("POST", baseURL+"/api/coachs/addcoach", reader)
@@ -46,6 +47,26 @@ func PostNewCoach(t *testing.T) {
 	if res.StatusCode != 201 {
 		t.Errorf("Success expected: %d", res.StatusCode)
 	}
+
+	if l.Lname != "Lariihhhiibi" {
+		t.Error("LName expected: ", l.Lname)
+	}
+
+	if l.Fname != "MEHEHHEHEHEDi" {
+		t.Error("FName expected: ", l.Fname)
+	}
+
+	if l.Actif != "true" {
+		t.Error("Actif expected: ", l.Actif)
+	}
+
+	if l.Email != "Mehdi@hotmale.com" {
+		t.Error("Actif expected: ", l.Actif)
+	}
+
+	if l.SeasonID != "1" {
+		t.Error("SeasonID expected: ", l.SeasonID)
+	}
 }
 
 //GetCoaches : test de la methode GET
@@ -58,9 +79,23 @@ func GetCoaches(t *testing.T) {
 		t.Error(err)
 	}
 
-	if res.StatusCode != 200 {
-		t.Errorf("Get coachs not a success: %d", res.StatusCode)
+	bodyBuffer, _ := ioutil.ReadAll(res.Body)
+
+	var co []api.Coaches
+	err = json.Unmarshal(bodyBuffer, &co)
+	if err != nil {
+		t.Error(err)
 	}
+
+	if res.StatusCode != 200 {
+		t.Errorf("Response code expected: %d", res.StatusCode)
+	}
+
+	// On s'assure qu'il y ait au moins un coach
+	if len(co) <= 0 {
+		t.Errorf("Number of teams expected: %d", len(co))
+	}
+
 }
 
 //UpdateCoach : test de la methode PUT
@@ -75,5 +110,62 @@ func UpdateCoach(t *testing.T) {
 
 	if res.StatusCode != 201 {
 		t.Errorf("Success expected: %d", res.StatusCode)
+	}
+}
+
+// TestModifierCoach test la modification de l'équipe créée plus haut
+func TestModifierCoach(t *testing.T) {
+	reader = strings.NewReader(`{"Fname": "Mehdi", "Lname": "Laribi", "Actif": "false", "Email": "Mehdi@hotmale.com"}`)
+
+	// rmID est utilisé ici pour permettre la modification créée plus haut
+	request, err := http.NewRequest("PUT", baseURL+"/api/coaches/editcoach/"+rmID, reader)
+	res, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	bodyBuffer, _ := ioutil.ReadAll(res.Body)
+
+	var te api.Coaches
+	err = json.Unmarshal(bodyBuffer, &te)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 201 {
+		t.Errorf("Response code expected: %d", res.StatusCode)
+	}
+
+	if te.Fname != "Mehdi" {
+		t.Error("New name expected: ", te.Fname)
+	}
+
+	if te.Lname != "Laribi" {
+		t.Error("Last name expected: ", te.Lname)
+	}
+}
+
+// TestCreerCoacheVide test que créer un coach sans
+// information. Doit retourner une erreur.
+func TestCreerCoacheVide(t *testing.T) {
+	reader = strings.NewReader(``)
+
+	request, err := http.NewRequest("POST", baseURL+"/api/coachs/addcoach", reader)
+	res, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 400 {
+		t.Errorf("Response code expected: %d", res.StatusCode)
+	}
+
+	var me MessageError
+	responseMapping(&me, res)
+
+	if !strings.Contains(me.Err, "Veuillez remplir tous les champs.") {
+		t.Errorf("Error expected: %s", me.Err)
 	}
 }
